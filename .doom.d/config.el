db13ddf95188558a062e04d3102746ab55ae5baf
Ivan Yakushev <iyakushev@quantumbrains.com>
2021-11-16 17:08:26 +0300

    Update emacs config

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -22,11 +22,10 @@
 ;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
 ;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
 
-
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-opera)
+(setq doom-theme 'doom-one)
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
@@ -34,9 +33,21 @@
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
-(setq display-line-numbers-type t)
+(setq display-line-numbers-type 'relative)
 
 
+;; set treemacs settings
+(setq doom-themes-treemacs-theme "doom-colors")
+(setq treemacs-git-mode 'extended)
+
+
+;; disable popup ui docs
+(setq lsp-ui-doc-enable 'nil)
+
+;; set rust-analyzer as default
+(setq rustic-lsp-server 'rust-analyzer)
+(after! rustic
+  (setq lsp-rust-server 'rust-analyzer))
 ;; Here are some additional functions/macros that could help you configure Doom:
 ;;
 ;; - `load!' for loading external *.el files relative to this one
